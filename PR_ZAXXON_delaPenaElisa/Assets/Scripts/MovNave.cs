4073f3b5d6c197bd0ef68d6e9f601c6107f2dcf6
Eli <77675394+edelap00@users.noreply.github.com>
2021-09-29 10:17:24 +0200

    Principio de restricción
    Pequeños ajustes de escena y el principio del código de restricción de hoy

--- PR_ZAXXON_delaPenaElisa/Assets/Scripts/MovNave.cs
+++ PR_ZAXXON_delaPenaElisa/Assets/Scripts/MovNave.cs
@@ -8,6 +8,10 @@ public class MovNave : MonoBehaviour
     //variables
     public float speedX = 5.0f;
     public float speedY = 3.0f;
+    float limRight = 10;
+    float limLeft = 10;
+    float limUp = 10;
+    float limDown = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,12 +23,23 @@ public class MovNave : MonoBehaviour
     void Update()
     {
         //mov eje 1
-        float DesplY = Input.GetAxis("Vertical") * speedY;
-        transform.Translate(Vector3.up * DesplY * Time.deltaTime);
+        float desplY = Input.GetAxis("Vertical") * speedY;
+        transform.Translate(Vector3.up * desplY * Time.deltaTime);
 
         //mov eje 2
-        float DesplX = Input.GetAxis("Horizontal") * speedX;
-        transform.Translate(Vector3.right * DesplX * Time.deltaTime);
+        float desplX = Input.GetAxis("Horizontal") * speedX;
+        transform.Translate(Vector3.right * desplX * Time.deltaTime);
+
+        //código para restricción
+
+        float posX = transform.position.x;
+        float posY = transform.position.y;
+        
+        if(posX>=limRight && desplX > 0)
+        {
+            desplX = 0;
+        } else { }
+
 
     }
 }
